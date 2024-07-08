6fc83032cde1be1d625f102de37aa79d7c568366
fuceda <fransanchezuceda@gmail.com>
2024-07-08 18:41:36 +0200

    init porject

---
+++ src/main/java/software_libre/api_luna/ApiLunaApplication.java
@@ -0,0 +1,13 @@
+package software_libre.api_luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/software_libre/api_luna/ApiLunaApplicationTests.java
@@ -0,0 +1,13 @@
+package software_libre.api_luna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
